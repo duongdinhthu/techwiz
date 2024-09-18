1c0b5f57c362fb79eaaf891e908d7fff214dca16
Dương Thứ <143236113+duongdinhthu@users.noreply.github.com>
2024-09-18 16:15:34 +0700

    ok

---
+++ src/main/java/com/project/esavior/controller/HospitalController.java
@@ -0,0 +1,25 @@
+package com.project.esavior.controller;
+
+import com.project.esavior.model.Hospital;
+import com.project.esavior.service.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/drivers")
+public class HospitalController {
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Hospital>> getAllHospitals() {
+        List<Hospital> hospitals = hospitalService.getAllHospitals();
+        return ResponseEntity.ok(hospitals);
+    }
+}

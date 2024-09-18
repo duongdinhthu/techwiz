92a00bc00a711f3bac9d24d22dc88630cf45e1a0
Dương Thứ <143236113+duongdinhthu@users.noreply.github.com>
2024-09-18 16:16:15 +0700

    ok

--- src/main/java/com/project/esavior/controller/HospitalController.java
+++ src/main/java/com/project/esavior/controller/HospitalController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/drivers")
+@RequestMapping("/api/hospitals")
 public class HospitalController {
 
     @Autowired
